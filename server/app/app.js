59164b469dcda44b2ab9864c21c498e20d3809db
Willem Odendaal <willem@newmedialabs.co.za>
2015-05-02 13:35:06 +0200

    Express now serving the index page.

--- server/app/app.js
+++ server/app/app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
-});
+app.use(express.static('../client/src'));
 
 var server = app.listen(3000, function() {
     var host = server.address().address;
